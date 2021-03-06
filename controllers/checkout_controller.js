d26725a30c0984f8bd69508c30b6322064dfefdb
NelsonBaileyIII <nelsonbailey@Nelsons-MBP.hsd1.ga.comcast.net>
2021-03-06 12:57:00 -0500

    conflicts issues

--- controllers/checkout_controller.js
+++ controllers/checkout_controller.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var ejs = require('ejs');
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var ejs = require('ejs');
 
-var path = require('path');
-var CheckoutRoutes = express.Router();
+// var path = require('path');
+// var CheckoutRoutes = express.Router();
 
-CheckoutRoutes.get('/checkout',function(req,res){
-  var order =  db.orders.findOne({where: {createdAt:'2021-03-06T17:29:55.840Z'}})
-.then(function (order){
-    console.log(order)
-    res.render('/checkout/checkout.ejs', {
-        order: order
-    });
-})
+// CheckoutRoutes.get('/checkout',function(req,res){
+//   var order =  db.orders.findOne({where: {createdAt:'2021-03-06T17:29:55.840Z'}})
+// .then(function (order){
+//     console.log(order)
+//     res.render('/checkout/checkout.ejs', {
+//         order: order
+//     });
+// })
 
-})
+// })
                                             
     
   
 
 
 
-module.exports = {"CheckoutRoutes" : CheckoutRoutes};
+// module.exports = {"CheckoutRoutes" : CheckoutRoutes};
